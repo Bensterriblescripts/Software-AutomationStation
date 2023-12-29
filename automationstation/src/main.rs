1dbafab22f11ece564a5ff855263fade7220143a
Ben <104821788+Bensterriblescripts@users.noreply.github.com>
2023-12-29 16:25:59 +1300

    started on queries, moved loop inside, needs work

--- automationstation/src/main.rs
+++ automationstation/src/main.rs
@@ -41,93 +41,84 @@ fn monitor_process() -> Result<(), Error> {
 
     let mut hashmap_processes: HashMap<String, f32> = HashMap::new();
 
-    // for _x in 1..1441 {
-    //     let vec_returnedlist = process_scan(&reg_process, &reg_format);
-    //     for returnedlist in vec_returnedlist {
+    for _x in 1..1441 {
+        let vec_returnedlist = process_scan(&reg_process, &reg_format);
+        for returnedlist in vec_returnedlist {
 
-    //         // Weight calculation - 8 hours of uptime is '1', everything else is a median
-    //         let weight: f32 = 0.0020833;
-    //         if hashmap_processes.contains_key(&returnedlist) {
-    //             *hashmap_processes.get_mut(&returnedlist).unwrap() += weight;
-    //             println!("Update to existing: {:?} with a new weight of: {:?}", &returnedlist, hashmap_processes.get(&returnedlist).unwrap());
-    //         }
-    //         else {
-    //             println!("Adding : {:?}", &returnedlist);
-    //             hashmap_processes.insert(
-    //                 returnedlist,
-    //                 weight,
-    //             );
-    //         }
-    //     }
-
-    //     thread::sleep(Duration::from_secs(60));
-    // }
-    
-    // Get the current unix time
-    let start = SystemTime::now();
-    let since_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards");
-    let seconds_since_epoch = since_epoch.as_secs();
-    println!("Unixtimestamp: {:?}", seconds_since_epoch);
-
-    // Get the env variables
-    let pg_user = match env::var_os("PG_USER") {
-        Some(v) => v.into_string().unwrap(),
-        None => panic!("$USER is not set")
-    };
-    let pg_pass = match env::var_os("PG_PASS") {
-        Some(v) => v.into_string().unwrap(),
-        None => panic!("$PASS is not set")
-    };
-
-    // DB Storage for processes
-    let encodedpass = encode(&pg_pass);
-    let conn_string = format!("postgresql://{}:{}@localhost/postgres", pg_user, encodedpass);
-
-    let mut client = match Client::connect(
-        &conn_string,
-        NoTls,
-    ) {
-        Ok(client) => client,
-        Err(err) => {
-            eprintln!("Error connecting to the database: {}", err);
-            return Err(err);
+            // Weight calculation - 8 hours of uptime is '1', everything else is a median
+            let weight: f32 = 0.0020833;
+            if hashmap_processes.contains_key(&returnedlist) {
+                *hashmap_processes.get_mut(&returnedlist).unwrap() += weight;
+                println!("Update to existing: {:?} with a new weight of: {:?}", &returnedlist, hashmap_processes.get(&returnedlist).unwrap());
+            }
+            else {
+                println!("Adding : {:?}", &returnedlist);
+                hashmap_processes.insert(
+                    returnedlist,
+                    weight,
+                );
+            }
         }
-    };
-    let result = client.query("SELECT * FROM process", &[]);
-    match result {
-        Ok(rows) => {
-            for row in rows {
-                let name: &str = row.get(0);
-                let weight: f32 = row.get(1);
-                let lastactive: i64 = row.get(2);
-                let firstactive: i64 = row.get(3);
-                
-                println!("Found row: {:?}, {:?}, {:?}, {:?}", name, weight, lastactive, firstactive);
+        // Get the current unix time
+        let start = SystemTime::now();
+        let since_epoch = start
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards");
+        let seconds_since_epoch: i64 = since_epoch.as_secs().try_into().unwrap();
+        println!("Unixtimestamp: {:?}", seconds_since_epoch);
+
+        // Get the env variables
+        let pg_user = match env::var_os("PG_USER") {
+            Some(v) => v.into_string().unwrap(),
+            None => panic!("$USER is not set")
+        };
+        let pg_pass = match env::var_os("PG_PASS") {
+            Some(v) => v.into_string().unwrap(),
+            None => panic!("$PASS is not set")
+        };
+
+        // DB Storage for processes
+        let encodedpass = encode(&pg_pass);
+        let conn_string = format!("postgresql://{}:{}@localhost/postgres", pg_user, encodedpass);
+
+        let mut client = match Client::connect(
+            &conn_string,
+            NoTls,
+        ) {
+            Ok(client) => client,
+            Err(err) => {
+                eprintln!("Error connecting to the database: {}", err);
+                return Err(err);
+            }
+        };
+        let result = client.query("SELECT * FROM process", &[]);
+        match result {
+            Ok(rows) => {
+                for row in rows {
+                    let name: &str = row.get(0);
+                    let weight: f32 = row.get(1);
+                    let lastactive: i64 = row.get(2);
+                    let firstactive: i64 = row.get(3);
+                    
+                    println!("Found row: {:?}, {:?}, {:?}, {:?}", name, weight, lastactive, firstactive);
 
                     // Updating a known process
                     if hashmap_processes.contains_key(name) {
                         let new_weight = hashmap_processes.get(name).unwrap();
-                        let new_lastactive = seconds_since_epoch;
+
+                        client.execute("UPDATE process SET weight = $1 AND lastactive = $2 WHERE name = $3", &[&new_weight, &seconds_since_epoch, &name],)?;
+
                         hashmap_processes.remove(name);
                     }
-                    // Adding a new process to the DB
-                    else {
-
-                    }
-
-            };
-        }
-        Err(err) => {
-            eprintln!("Error during select query: {}", err);
-            return Err(err);
+                };
+            }
+            Err(err) => {
+                eprintln!("Error during select query: {}", err);
+                return Err(err);
+            }
         }
     }
-
     Ok(())
-
-    // Store our processes and weights
 
 }
 fn process_scan(reg_process: &Regex, reg_format: &Regex) -> Vec<String> {
